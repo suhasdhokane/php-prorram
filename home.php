5ae798684854f7b999b7e140b92f6cf48af64e50
suhas dhokane <65722084+suhasdhokane@users.noreply.github.com>
2020-08-03 19:31:59 +0530

    new file

---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<?php
+ $name="Suhas";
+ ?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>hello word</title>
+  </head>
+  <body>
+    <h1> hello <?php echo  $name; ?> </h1>
+  </body>
+</html>
